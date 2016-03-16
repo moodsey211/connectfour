4b017a00d1f073bc7b197cf9b099e4be2266e2b2
Ismael C. Cristal Jr <icristal@treon-inc.com>
2016-03-16 09:04:55 +0800

    Changing remote database host

--- webapp/conf/constants.php
+++ webapp/conf/constants.php
@@ -6,7 +6,7 @@
 define('SITENAME', 'ConnectFour');
 
 if($_SERVER['HTTP_HOST'] != 'localhost') {
-    define('DBHOST', 'localhost');
+    define('DBHOST', 'ec2-107-21-101-67.compute-1.amazonaws.com');
     define('DBNAME', 'd4158a4bg2bf2j');
     define('DBUSER', 'cfopkagjzicpdx');
     define('DBPASS', 'ZidhZKcgyOqV71WZDpmjGkcRTg');
